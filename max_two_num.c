ae954927bf110c25eb8e8eb48b33c6d539ed2b7d
Bhumit Javiya <javiyabhumit54@gmail.com>
2021-10-30 22:05:50 +0530

    check max from 2 num

---
+++ max_two_num.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main() {
+
+    int a, b;
+    printf("Enter the value of a\n");
+    scanf("%d", &a);
+
+    printf("Enter the value of b\n");
+    scanf("%d", &b);
+
+    if (a > b) {
+        printf("a is max\n");
+    }
+    else {
+        printf("b is max\n");
+    }
+
+    return 0;
+
+}
