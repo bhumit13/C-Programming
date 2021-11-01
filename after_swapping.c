5d4181c34c9e420eb102d70c15f516e9d61088e3
Bhumit Javiya <javiyabhumit54@gmail.com>
2021-11-01 14:38:11 +0530

    enter the value and swap this value

---
+++ after_swapping.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+ 
+int main()
+{
+    int a, b, c;
+    printf("Enter the value of a\n");
+    scanf("%d",  &a);
+    
+    printf("Enter the value of b\n");
+    scanf("%d",  &b);
+    
+    c=a;
+    a=b;
+    b=c;
+    
+    printf("After swapping\n");
+    printf("%d\n", a);
+    printf("%d\n", b);
+    
+    return 0;
+}
+
