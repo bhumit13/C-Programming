43b019ebfcb0eeccfed9b38ea60e4adfd5d1ffc9
Bhumit Javiya <javiyabhumit54@gmail.com>
2021-11-03 13:18:00 +0530

    convert celsius tempreture to fahrenheit

--- celsius_fahrenheit.c
+++ celsius_fahrenheit.c
@@ -2,7 +2,14 @@
 
 int main() {
     
-    int 
+    float celsius, far;
+
+    printf("Enter the value of celsius\n");
+    scanf("%f", &celsius);
+
+    far= (celsius*9/5)+32;
+
+    printf("The value of this celsius temperature in Fahrenheit is %f\n", far);
     
     return 0;
     
