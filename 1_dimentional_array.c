94374281fabfa0a152284b1535096dc940cac005
Bhumit Javiya <javiyabhumit54@gmail.com>
2021-11-21 20:00:44 +0530

    print 1-dimentional array for 5-elements

---
+++ 1_dimentional_array.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main() {
+    
+    int a[5], i;
+    for (i = 0; i < 5; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+    for (i = 0; i < 5; i++)
+    {
+        printf("Array element is %d\n", a[i]);
+    }
+    
+    
+
+    return 0;
+    
+}
