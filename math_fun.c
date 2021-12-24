91cf4120424737d4957b742c60f8aad185f47374
Bhumit Javiya <javiyabhumit54@gmail.com>
2021-12-24 12:48:49 +0530

    maths function value print

---
+++ math_fun.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<math.h>
+
+int main() {
+    
+    printf("Ceiling function : %f\n", ceil(3.6));
+    printf("Ceiling function : %f\n", ceil(3.3));
+    printf("floor function : %f\n", floor(3.6));
+    printf("floor function : %f\n", floor(3.3));
+    printf("Square function : %f\n", sqrt(16));
+    printf("Power function : %f\n", pow(2,3));
+    printf("absolute function : %d\n", abs(-12));
+
+    return 0;
+    
+}
